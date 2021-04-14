f305ac3443901c1d21333f103c7d397c6b791cfc
Hidetake Iwata <int128@gmail.com>
2021-04-14 10:47:47 +0900

    Improve logs (#12)

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,17 @@ import {createRepositoryIfNotExist, putLifecyclePolicy} from './ecr'
 async function run(): Promise<void> {
   try {
     const repositoryName = core.getInput('repository')
+    const lifecyclePolicyPath = core.getInput('lifecycle-policy')
+
+    core.startGroup(`Create repository ${repositoryName} if not exist`)
     const repository = await createRepositoryIfNotExist(repositoryName)
     core.setOutput('repository-uri', repository.repositoryUri)
+    core.endGroup()
 
-    const lifecyclePolicyPath = core.getInput('lifecycle-policy')
     if (lifecyclePolicyPath !== '') {
+      core.startGroup(`Put the lifecycle policy to repository ${repositoryName}`)
       await putLifecyclePolicy(repositoryName, lifecyclePolicyPath)
+      core.endGroup()
     }
   } catch (error) {
     core.setFailed(error.message)
